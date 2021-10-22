4dddcf7dc6949fd3dc455aeb008f4305273eeb97
정범진 <last9500@naver.com>
2021-10-22 10:24:20 +0900

    [BE} chore: 통합 빌드 환경설정

--- backend/src/main/java/com/diary/config/WebMvcConfig.java
+++ backend/src/main/java/com/diary/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package com.diary.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -8,20 +9,21 @@ import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.io.IOException;
 
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver() {
-                    @Override
-                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                        Resource requestResource = location.createRelative(resourcePath);
-                        return requestResource.exists() && requestResource.isReadable()
-                                ? requestResource : new ClassPathResource("/static/index.html");
-                    }
-                });
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**")
+//                .addResourceLocations("classpath:/static/")
+//                .resourceChain(true)
+//                .addResolver(new PathResourceResolver() {
+//                    @Override
+//                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+//                        Resource requestResource = location.createRelative(resourcePath);
+//                        return requestResource.exists() && requestResource.isReadable()
+//                                ? requestResource : new ClassPathResource("/static/index.html");
+//                    }
+//                });
+//    }
 }
