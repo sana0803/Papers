87786a7dfde813f8c0a9275a57a53c2e1bda6fcb
이재영 <giver12@naver.com>
2021-10-29 17:59:32 +0900

    [BE] feature: 회원가입, 로그인 API 구현

---
+++ backend/src/main/java/com/diary/api/controller/AuthController.java
@@ -0,0 +1,30 @@
+package com.diary.api.controller;
+
+import com.diary.api.request.UserLoginReq;
+import com.diary.api.response.BaseResponseBody;
+import com.diary.api.response.UserRes;
+import com.diary.api.service.UserService;
+import com.diary.common.util.JwtTokenUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping()
+    public ResponseEntity<? extends BaseResponseBody> login(@RequestBody UserLoginReq userLoginReq){
+        UserRes userRes = userService.authenticate(userLoginReq);
+        if (userRes == null) return ResponseEntity.status(401).body(BaseResponseBody.of(401, "로그인 인증실패"));
+        userRes.setStatusCode(200);
+        userRes.setMessage("로그인 되었습니다.");
+        return ResponseEntity.ok(userRes);
+    }
+}
--- backend/src/main/java/com/diary/api/service/UserService.java
+++ backend/src/main/java/com/diary/api/service/UserService.java
@@ -1,9 +1,12 @@
 package com.diary.api.service;
 
 import com.diary.api.db.entity.User;
-import com.diary.api.request.UserReq;
+import com.diary.api.request.UserLoginReq;
+import com.diary.api.request.UserSignupReq;
+import com.diary.api.response.UserRes;
 
 public interface UserService {
-    User createUser(UserReq userReq);
+    boolean signupUser(UserSignupReq userSignupReq);
     User getUserByUserId(String userId);
+    UserRes authenticate(UserLoginReq userLoginReq);
 }
