8bbfc82294f1b2ff58a4cbd69af0ecdc6385d0d5
정범진 <last9500@naver.com>
2021-11-12 10:12:22 +0900

    [BE] test: kakao API test

---
+++ backend/src/main/java/com/diary/api/request/KakaoReq.java
@@ -0,0 +1,15 @@
+package com.diary.api.request;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class KakaoReq {
+    String id;
+    String pwd;
+    List<MultipartFile> imageList;
+}
